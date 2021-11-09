7c0cdc65220493fd0828e56b6be033f352714aab
Lance Huang <lance.huang@deltaww.com>
2021-11-09 16:33:31 +0800

    #spring logging

---
+++ spring-application/src/main/java/idv/lance/runner/App1Runner.java
@@ -0,0 +1,15 @@
+package idv.lance.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class App1Runner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+//        System.out.println("application runner");
+    }
+}

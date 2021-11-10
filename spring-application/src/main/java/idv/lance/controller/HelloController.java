62dea36b46feae21bba2df138803caa0092ac8c5
Lance Huang <lance.huang@deltaww.com>
2021-11-10 11:22:21 +0800

    #spring i18n messages source

--- spring-application/src/main/java/idv/lance/controller/HelloController.java
+++ spring-application/src/main/java/idv/lance/controller/HelloController.java
@@ -1,10 +1,20 @@
 package idv.lance.controller;
 
 import idv.lance.service.HelloService;
+import idv.lance.vo.NameValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
 @Slf4j
 @RestController
 public class HelloController {
@@ -12,8 +22,21 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
+    @Autowired
+    MessageSource messageSource;
+
     @GetMapping("/hello")
     public String hello() {
         return helloService.sayHello();
+    }
+
+    @RequestMapping(value = {"/title"}, method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public ResponseEntity<NameValue> changeLocale(HttpServletRequest request, HttpServletResponse response) {
+        Locale locale = LocaleContextHolder.getLocale();
+        log.info("locale {}", locale);
+        log.info("app.title {}", messageSource.getMessage("app.title", null, locale));
+        NameValue nameValue = new NameValue("title", messageSource.getMessage("app.title", null, locale));
+        return new ResponseEntity<NameValue>(nameValue, HttpStatus.OK);
     }
 }
---
+++ spring-application/src/main/java/idv/lance/vo/NameValue.java
@@ -0,0 +1,27 @@
+package idv.lance.vo;
+
+public class NameValue {
+    private String key;
+    private String value;
+
+    public NameValue(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}

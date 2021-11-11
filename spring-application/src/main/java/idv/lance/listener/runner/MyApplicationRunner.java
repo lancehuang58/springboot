195e981016d6bab1e9eaf3c67bc42b75cedce764
Lance Huang <LanceHuang79@gmail.com>
2021-11-11 21:02:22 +0800

    runner

--- spring-application/src/main/java/idv/lance/listener/runner/MyApplicationRunner.java
+++ spring-application/src/main/java/idv/lance/listener/runner/MyApplicationRunner.java
@@ -5,7 +5,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ClassUtils;
+
 
 //--Mygroup=a,b --filePath=c:/data/1.txt -myargs=2 testNoArg TestNoarg2=3
 @Slf4j
@@ -15,17 +15,6 @@ public class MyApplicationRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("run by my application runner");
-        printArgs(args);
-    }
-    public void printArgs(ApplicationArguments arguments) {
-        System.out.println("# NonOptionArgs: " + arguments.getNonOptionArgs().size());
-        System.out.println("# OptionName size: " + arguments.getOptionNames().size());
-        System.out.println("# NonOptionArgs:");
-        arguments.getNonOptionArgs().forEach(System.out::println);
-
-        System.out.println("# OptionNames:");
-        arguments.getOptionNames().forEach(optionName -> {
-            System.out.println("--" + optionName + "=" + arguments.getOptionValues(optionName));
-        });
+        
     }
 }
